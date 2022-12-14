946b3f9d5d2cfc4da99d65d75998f7bb5d30d6ff
Eray Aybek <aybek17@itu.edu.tr>
2022-12-14 23:58:20 +0300

    add project 6

---
+++ 06-CharacterCounter/index.js
@@ -0,0 +1,16 @@
+const textAreaEl = document.getElementById("textarea");
+const totalCounterEl = document.getElementById("total-counter");
+const remainingCounterEl = document.getElementById("remaining-counter");
+
+
+textAreaEl.addEventListener("keyup", () => {
+    console.log("key is pressed")
+    updateCounter();
+})
+
+updateCounter();
+
+function updateCounter(){
+    totalCounterEl.innerText = textAreaEl.value.length;
+    remainingCounterEl.innerText = textAreaEl.getAttribute("maxLength") - textAreaEl.value.length;
+}
