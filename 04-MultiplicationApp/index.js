27f777ea5339a89a442701025381fcf014773f5c
Eray Aybek <aybek17@itu.edu.tr>
2022-12-14 23:57:39 +0300

    add MultiplicationApp project

---
+++ 04-MultiplicationApp/index.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(10 * Math.random()); // yuvarlama islemi ile 0-10 arasi sayilar verecek.
+const num2 = Math.ceil(10 * Math.random());
+
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form")
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score){
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = +inputEl.value // + -> convert string to number
+    if (userAns === correctAns){
+        score++;
+        updateLocalStorage();
+    }else{
+        score--
+        updateLocalStorage();
+    }
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
