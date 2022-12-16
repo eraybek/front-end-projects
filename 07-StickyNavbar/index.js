86bd26b5024caddbaa02f7189ffd77b90ee43d3a
Eray Aybek <aybek17@itu.edu.tr>
2022-12-16 12:09:45 +0300

    add StickyNavbar project

---
+++ 07-StickyNavbar/index.js
@@ -0,0 +1,13 @@
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+
+
+window.addEventListener("scroll", () => {
+    
+    if (window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50){ /* 50px text margin */
+        navbarEl.classList.add("active");
+    }else{
+        navbarEl.classList.remove("active");
+    }
+});
